f4309775893d083a846f02480dcdf647b9ac2ca1
Zhanna <zhanna01992@gmail.com>
2023-01-31 12:52:29 +0200

    feat: divide html into separate components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0)
+  return (
+    <div class="categories">
+    <ul>
+      <li class="active">Все</li>
+      <li>Мясные</li>
+      <li>Вегетарианская</li>
+      <li>Гриль</li>
+      <li>Острые</li>
+      <li>Закрытые</li>
+    </ul>
+  </div>
+  )
+}
+
+export default Categories
