83302a39690a7109125e8f40902b6d10b9f65bfb
zhannach <zhanna.chaykovska@gmail.com>
2023-03-09 23:03:27 +0200

    feat: search optimization with lodash.debounce

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,21 +1,48 @@
-import React, { useContext } from 'react'
-import styles from './search.module.scss'
-import close from '../../assets/close.svg'
-import { SearchContext } from '../../App'
+import React, { useCallback, useContext, useRef, useState } from "react";
+import debounce from "lodash.debounce";
+import styles from "./search.module.scss";
+import close from "../../assets/close.svg";
+import { SearchContext } from "../../App";
 
 export default function Search() {
+  const [value, setValue] = useState("");
+  const inputRef = useRef();
+  const { setSearchValue } = useContext(SearchContext);
 
-  const {searchValue, setSearchValue} = useContext(SearchContext)
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    [],
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value)
+    updateSearchValue(event.target.value)
+  }
+  const onClickClear = () => {
+    setSearchValue("");
+    setValue("")
+    inputRef.current.focus();
+  };
+
   return (
-    <div className={styles.root}> 
-    <input className={styles.input} 
-    value={searchValue} 
-    onChange={(event) => setSearchValue(event.target.value)}
-    placeholder='search pizza'
-    ></input>
-    {searchValue && 
-        <img onClick={() => setSearchValue('')} className={styles.clearIcon} src={close} alt=''></img>}
-  </div>
-   
-  )
+    <div className={styles.root}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        value={value}
+        onChange={onChangeInput}
+        placeholder="search pizza"
+      ></input>
+      {value && (
+        <img
+          onClick={onClickClear}
+          className={styles.clearIcon}
+          src={close}
+          alt=""
+        ></img>
+      )}
+    </div>
+  );
 }
