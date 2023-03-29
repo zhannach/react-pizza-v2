ea736dd0a66bacd6ab7b0366f7c2120198292bb7
zhannach <zhanna.chaykovska@gmail.com>
2023-03-29 19:05:02 +0300

    feat: add CartEmpty component

---
+++ src/components/CartEmpty.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import cartEmptyImg from "../assets/empty-cart.png";
+
+export const CartEmpty = () => (
+  <div className="cart cart--empty">
+    <h2>
+      Cart is empty <span>😕</span>
+    </h2>
+    <p>
+      You probably haven't ordered pizza yet.
+      <br />
+      To order pizza, go to the main page.
+    </p>
+    <img src={cartEmptyImg} alt="Empty cart" />
+    <Link to="/" className="button button--black">
+      <span>Turn back</span>
+    </Link>
+  </div>
+);
